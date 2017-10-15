ccb279d7090ab366b10122b1645221d537fd365f
papapapapapapapa <r3386588@gmail.com>
2017-10-15 18:19:27 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner in=new Scanner(System.in);
+        float x;
+        x=in.nextFloat();
+        System.out.printf("%.1f",x/600);
+        System.out.println();
+    }
+}
